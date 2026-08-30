e93c61e365bce7d03ed6dc83386e1099b8713508
Pistan Renso <rensolpdp@gmail.com>
2026-08-30 14:11:08 -0300

    implemente componentes:Main,Card y sus respectivos modules.css, tambien modifique para que tenga modo oscuro el fondo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import './App.css'
+
+import Main from './components/main/Main.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+    <Main/>
     </>
   )
 }
