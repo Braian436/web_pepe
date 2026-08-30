055cc83bcd9431d619c7f39e98284ea23d23a987
unknown <zambracele@gmail.com>
2026-08-30 04:49:12 -0300

    Se agrego contenido del footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 // import { useState } from 'react'
 import './App.css'
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
-    </>
+      {/* Aquí va tu contenido principal */}
+      <Footer />
+    </div>
   )
 }
 
